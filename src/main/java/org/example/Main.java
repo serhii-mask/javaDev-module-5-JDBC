4d1a85a61f52f8e590cf262624c69a262f8925fe
Serhii <serhii.mask@gmail.com>
2023-10-17 09:08:32 +0300

    init: start hw 5

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.example.company.DatabaseInitService;
+import org.example.company.DatabasePopulateService;
+import org.example.company.DatabaseQueryService;
+import org.example.database.Database;
+
+public class Main {
+    public static void main(String[] args) {
+        DatabaseInitService.main(args);
+        DatabasePopulateService.main(args);
+
+        Database database = Database.getInstance();
+        DatabaseQueryService queryService = new DatabaseQueryService(database);
+
+        System.out.println("FindMaxSalaryWorker() = " + queryService.findMaxSalaryWorker());
+        System.out.println("FindMaxProjectsClient() = " + queryService.findMaxProjectsClient());
+        System.out.println("FindLongestProject() = " + queryService.findLongestProject());
+        System.out.println("FindYoungestEldestWorkers() = " + queryService.findYoungestEldestWorkers());
+        System.out.println("PrintProjectPrices() = " + queryService.printProjectPrices());
+
+        database.close();
+    }
+}
